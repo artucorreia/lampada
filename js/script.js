48e69d53ea26bef46d79555720bfc6c7a88cef49
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2022-11-08 11:20:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,29 @@
-let img = window.document.getElementById('imagem');
-let foto = window.document.getElementById('foto');
-img.setAttribute('id', 'foto');
+let img = window.document.getElementById('img');
+let quebrada = false;
+
+function on () {
+    if (quebrada != true) {
+        img.src = 'image/ligada.png';
+    }
+}
+
+function off () {
+    if (quebrada != true) {
+        img.src = 'image/desligada.png';
+    }
+}
+
+function quebrar () {
+    img.src = 'image/quebrada.png';
+    quebrada = true;
+}
 
 let botaoLigar = window.document.getElementById('on');
 botaoLigar.addEventListener('click', on);
-function on () {
-    if (quebrada != true) {
-        img.setAttribute('src', 'imagens/ligada.png');
-        foto.appendChild(img);
-        clicado = 0;
-    }
-}
 
 let botaoDesligar = window.document.getElementById('off');
 botaoDesligar.addEventListener('click', off);
-function off () {
-    if (quebrada != true) {
-        img.setAttribute('src', 'imagens/desligada.png');
-        foto.appendChild(img);
-        clicado = 0;
-    }
-}
 
-let clicado = 0;
-let quebrada = false;
-function bater () {
-    clicado++
-    if ( clicado >= 2 ) {
-        img.setAttribute('src', 'imagens/quebrada.png');
-        foto.appendChild(img);
-        quebrada = true;
-    }
-}
+img.addEventListener('mouseenter', on);
+img.addEventListener('mouseout', off);
+img.addEventListener('dblclick', quebrar);
